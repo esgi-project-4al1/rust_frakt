7eda6d65b9360ce7f5da3a928a26babdcf28a4e5
Aristide Fumo <aristide.fumo@sopht.com@sopht.com>
2023-12-22 08:35:08 +0100

    add mandelbrot, and julia

--- message/src/build_julia.rs
+++ message/src/build_julia.rs
@@ -1,7 +1,6 @@
 use crate::message::{JuliaDescriptor, Range, Resolution};
 
 impl JuliaDescriptor {
-
     fn calculate_escape_time(px: f64, py: f64, julia: &JuliaDescriptor, max_iteration: u32) -> u32 {
         let mut zx = px;
         let mut zy = py;
@@ -17,10 +16,52 @@ impl JuliaDescriptor {
         i
     }
 
-    fn map_escape_time_to_color(escape_time: u32, max_iteration: u32) -> u8 {
-        let color = (escape_time as f64 / max_iteration as f64) * 255.0;
-        color as u8
+    fn hsv_to_rgb(h: f64, s: f64, v: f64) -> u32 {
+        let c = v * s;
+        let x = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
+        let m = v - c;
+
+        let (r, g, b) = if h < 60.0 {
+            (c, x, 0.0)
+        } else if h < 120.0 {
+            (x, c, 0.0)
+        } else if h < 180.0 {
+            (0.0, c, x)
+        } else if h < 240.0 {
+            (0.0, x, c)
+        } else if h < 300.0 {
+            (x, 0.0, c)
+        } else {
+            (c, 0.0, x)
+        };
+
+        let r = ((r + m) * 255.0) as u32;
+        let g = ((g + m) * 255.0) as u32;
+        let b = ((b + m) * 255.0) as u32;
+
+        (r << 16) | (g << 8) | b
     }
+
+    fn map_escape_time_to_color(&self, escape_time: u32, max_iteration: u32) -> u8 {
+        if escape_time == max_iteration {
+            return 0; // Assigner une couleur spécifique pour les points qui ne divergent pas
+        }
+
+        let normalized_time = escape_time as f64 / max_iteration as f64;
+        let hue = 360.0 * normalized_time; // Variation de teinte de 0 à 360 degrés
+
+        // Convertir la teinte en RVB
+        let rgb_color = Self::hsv_to_rgb(hue, 1.0, 1.0);
+
+        // Extraire les composantes RVB
+        let red = (rgb_color >> 16) as u8;
+        let green = (rgb_color >> 8) as u8;
+        let blue = rgb_color as u8;
+
+        // Retourner la couleur RVB en niveaux de gris
+        ((red as f64 * 0.3) + (green as f64 * 0.59) + (blue as f64 * 0.11)) as u8
+    }
+
 
     pub fn calculate(&self, max_iteration: u16, resolution: Resolution, range: Range) -> Vec<u8> {
         return self.calculate_fractal_julia(max_iteration, resolution, range);
@@ -39,13 +80,56 @@ impl JuliaDescriptor {
 
                 let escape_time = Self::calculate_escape_time(px, py, self, max_iteration as u32);
 
-                let color = Self::map_escape_time_to_color(escape_time, max_iteration as u32);
+                let color = Self::map_escape_time_to_color(self, escape_time, max_iteration as u32);
                 pixels.push(color);
             }
         }
 
         pixels
     }
+}
+#[cfg(test)]
+mod tests {
+    use crate::img::save_fractal_image;
+    use super::*;
+    use crate::message::{Complex, Point};
+
+    #[test]
+    fn test_calculate_fractal_julia() {
+        let julia = JuliaDescriptor {
+            c: Complex { re: -0.9, im: 0.27015 },
+            divergence_threshold_square: 4.0,
+        };
+
+        let max_iteration = 1000;
+        let resolution = Resolution { nx: 800, ny: 600 };
+        let range = Range {
+            min: Point { x: -1.5, y: -1.0 },
+            max: Point { x: 1.5, y: 1.0 },
+        };
+
+        let pixels = julia.calculate_fractal_julia(max_iteration, resolution.clone(), range);
+        save_fractal_image(pixels.clone(), resolution, "test_fractal_julia.png");
+        assert_eq!(pixels.len(), 480000);
+    }
 
 
+    #[test]
+    fn test_calculate_fractal_julia_with_offset() {
+        let julia = JuliaDescriptor {
+            c: Complex { re: 0.285, im:  0.013 },
+            divergence_threshold_square: 4.0,
+        };
+
+        let max_iteration = 1000;
+        let resolution = Resolution { nx: 800, ny: 600 };
+        let range = Range {
+            min: Point { x: -1.5, y: -1.0 },
+            max: Point { x: 1.5, y: 1.0 },
+        };
+
+        let pixels = julia.calculate_fractal_julia(max_iteration, resolution.clone(), range);
+        save_fractal_image(pixels.clone(), resolution, "test_fractal_julia2.png");
+        assert_eq!(pixels.len(), 480000);
+    }
 }
---
+++ message/src/img.rs
@@ -0,0 +1,18 @@
+use image::{ImageBuffer, Rgb};
+use crate::message::Resolution;
+
+
+
+pub fn save_fractal_image(pixels: Vec<u8>, resolution: Resolution, filename: &str) {
+    let width = resolution.nx.clone() as u32;
+    let height = resolution.ny.clone() as u32;
+    let mut imgbuf = ImageBuffer::new(width, height);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let index = (y * width + x) as usize;
+        let intensity = pixels[index];
+        *pixel = Rgb([intensity, intensity, intensity]);
+    }
+
+    imgbuf.save(filename).unwrap();
+}
